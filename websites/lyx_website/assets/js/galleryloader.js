d46da41e11f2da8ebf1c8b26d4fb0ef24db66392
andy-and-terry <yucheng.lee29@gmail.com>
2026-06-01 13:28:35 +0800

    Create galleryloader.js

---
+++ websites/lyx_website/assets/js/galleryloader.js
@@ -0,0 +1,15 @@
+// /* EDITING ELEMENT: [] */
+// const example = document.getElementById("");
+// example.innerHTML = "";
+/* EDITING ELEMENT: [artworks-last-updated] */
+const lastUpdated = document.getElementById("artworks-last-updted");
+lastUpdated.innerHTML = "00:00:00 00/00/0000";
+/* EDITING ELEMENT: [artworks-count] */
+const artworksCount = document.getElementById("artworks-count");
+artworksCount.innerHTML = "0";
+/* EDITING ELEMENT: [artworks-total-size] */
+const artworksSize = document.getElementById("artworks-total-size");
+artworksSize.innerHTML = "0KB/0MB/0GB";
+/* EDITING ELEMENT: [artworks-cost] */
+const artworksCost = document.getElementById("artworks-cost");
+artworksCost.innerHTML = "$0.00 [SGD]";
