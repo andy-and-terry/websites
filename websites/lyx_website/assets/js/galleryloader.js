1ad2ee7508f9e6955605c15caac5ebc281a0b74b
andy-and-terry <yucheng.lee29@gmail.com>
2026-06-01 13:42:54 +0800

    Wrap gallery data in paragraph tags

--- websites/lyx_website/assets/js/galleryloader.js
+++ websites/lyx_website/assets/js/galleryloader.js
@@ -3,13 +3,16 @@
 // example.innerHTML = "";
 /* EDITING ELEMENT: [artworks-last-updated] */
 const lastUpdated = document.getElementById("artworks-last-updted");
-lastUpdated.innerHTML = "00:00:00 00/00/0000";
+lastUpdated.innerHTML = "<p>00:00:00 00/00/0000</p>";
 /* EDITING ELEMENT: [artworks-count] */
 const artworksCount = document.getElementById("artworks-count");
-artworksCount.innerHTML = "0";
+artworksCount.innerHTML = "<p>0</p>";
 /* EDITING ELEMENT: [artworks-total-size] */
 const artworksSize = document.getElementById("artworks-total-size");
-artworksSize.innerHTML = "0KB/0MB/0GB";
+artworksSize.innerHTML = "<p>0KB/0MB/0GB</p>";
 /* EDITING ELEMENT: [artworks-cost] */
 const artworksCost = document.getElementById("artworks-cost");
-artworksCost.innerHTML = "$0.00 [SGD]";
+artworksCost.innerHTML = "<p>$0.00 [SGD]</p>";
+/* EDITING ELEMENT: [artworks-data] */
+const artworksData = document.getElementById("artworks-data");
+artworksData.innerHTML = "<p>None</p>";
