515ad5306e5109b0f307565b4aaa1949d00a9fa3
andy-and-terry <yucheng.lee29@gmail.com>
2026-06-04 21:57:21 +0800

    Create loader.js

---
+++ websites/photolibrary/assets/js/loader.js
@@ -0,0 +1,7 @@
+document.getElementById('main-list').innerHTML = `
+  <ul>
+    <li><a href="images/loader.html?search=all">All images</a></li>
+    <li><a href="images/loader.html?search="nature">Nature</a></li>
+    <li>Item 3</li>
+  </ul>
+`;
