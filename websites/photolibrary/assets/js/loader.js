e4de493a43cd88f99b4dd355b6609144ab8242ae
andy-and-terry <yucheng.lee29@gmail.com>
2026-06-05 07:35:11 +0800

    Update loader.js

--- websites/photolibrary/assets/js/loader.js
+++ websites/photolibrary/assets/js/loader.js
@@ -1,7 +1,8 @@
 document.getElementById('main-list').innerHTML = `
   <ul>
     <li><a href="images/loader.html?search=all">All images</a></li>
-    <li><a href="images/loader.html?search="nature">Nature</a></li>
-    <li>Item 3</li>
+    <li><a href="images/loader.html?search=nature">Nature</a></li>
+    <li><a href="images/loader.html?search=things">Things</a></li>
+    <li><a href="images/loader.html?search=animals">Animals</a></li>
   </ul>
 `;
